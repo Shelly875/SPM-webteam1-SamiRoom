10f1a6f53a45e836deba6d6bfd501d1553ecf4d7
Shelly Miron <shelly875@gmail.com>
2020-05-10 14:50:42 +0300

    changes in html files and js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 
 const EXPRESS = require('express');
+// const DB = require('./js/db-func');
 
 const APP_PORT = process.env.PORT || 3000;
 const APP = EXPRESS();
 const router = EXPRESS.Router();
 const PATH = __dirname;
 
-// all pages in the web
 APP.use(EXPRESS.static(__dirname));
+// DB.takeApartImages(1);
 
 router.get('/home', (req, res) => {
   res.sendFile(`${PATH}/index.html`);
@@ -40,7 +40,6 @@ router.get('/details', (req, res) => {
   res.sendFile(`${PATH}/apartment-detail.html`);
 });
 
-
 APP.use('/', router);
 
 APP.use('*', (req, res) => {
@@ -53,5 +52,4 @@ APP.get('/', (req, res) => {
 
 APP.listen(APP_PORT);
 // eslint-disable-next-line no-console
-console.log(`app is running on port: ${APP_PORT}`);
-console.log(`http://127.0.0.1: ${APP_PORT}/`);
+console.log(`app is running. port: ${APP_PORT}`, '/n', `http://127.0.0.1:${APP_PORT}/`);
